a3f6b620800d11e0269fb9fc79d54ee0448b27b6
alexey <djkah11@yandex.ru>
2016-04-12 02:11:42 +0300

    migrate to django

--- ask_kapustin/ask_kapustin_app/views.py
+++ ask_kapustin/ask_kapustin_app/views.py
@@ -1,7 +1,9 @@
+# coding=utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
@@ -21,14 +23,60 @@ def get_post_params(request):
         if request.GET.urlencode() != '':
             result.append('Get data:')
             for key, value in request.GET.items():
-                keyvalue=key+" = "+value
+                keyvalue = key + " = " + value
                 result.append(keyvalue)
 
     return HttpResponse('<br>'.join(result))
 
+
 def base(request):
-    return render(request,"base.html")
+    return render(request, "base.html")
+
+
+questions = []
+for i in xrange(3):
+    questions.append({
+        'title': 'Question #{}. How I can to do smth?'.format(i),
+        'body': 'Badges are numerical indicators of how many items are associated with a link. Use the .badge class within span elements to create badges.',
+        'nickname': "Никнейм_{}".format(i),
+        'id': i,
+    })
 
 
 def index(request, page):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'questions': questions,}, page)
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def question(request, question_id):
+    context = RequestContext(request, {
+        'que_id': question_id,
+    })
+    return render(request, 'question.html', {'questions': questions, "context": context})
+
+
+def hot(request):
+    return render(request, 'hot.html', {'questions': questions,})
+
+
+def tag(request, htag, page):
+    context = RequestContext(request, {
+        'hash_tag': htag,
+        "n_page": page,
+    })
+    return render(request, 'tag.html', {'questions': questions, "context": context})
+
+
+def error(request):
+    return render(request, '404page.html')
