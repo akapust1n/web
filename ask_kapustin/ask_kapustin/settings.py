f1f2e44d402fbbe8fb770bb974b023b3789fdfb1
alexey <djkah11@yandex.ru>
2016-04-24 01:13:31 +0300

    fix bug in setting.py (mystery)

--- ask_kapustin/ask_kapustin/settings.py
+++ ask_kapustin/ask_kapustin/settings.py
@@ -1,30 +1,28 @@
 """
 Django settings for ask_kapustin project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ')bkf4&&b7_-d&(f23al%196sxvec-e*@0%r%ket)s5un_zj17i'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','ask']
+
 
 
 # Application definition
@@ -36,7 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.core.paginator',
+    'ask_kapustin_app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -44,27 +42,22 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'ask_kapustin.urls'
 
+
 WSGI_APPLICATION = 'ask_kapustin.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -78,10 +71,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = "/static/"
 STATIC_URL = '/static/'
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
-
--- ask_kapustin/ask_kapustin_app/models.py
+++ ask_kapustin/ask_kapustin_app/models.py
@@ -1,15 +1,14 @@
-
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
+from django.utils import timezone
+
 
 class Question(models.Model):
-	autor=models.ForeignKey(User)
-	title = models.CharField(max_length=50)
-	text = models.TextField()
-	timeStamp = models.DateTimeField(default=datetime.datetime.now())
-	rating=models.IntegerField()
-	answerCount = models.IntegerField(default=0)
-
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    timeStamp = models.DateTimeField(default=timezone.now())
+    rating = models.IntegerField()
+    answerCount = models.IntegerField(default=0)
 
 # Create your models here.
